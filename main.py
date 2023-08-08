73a34dc74e2565010ea457ef48acf5b1f5e1b4f0
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-08-09 02:15:46 +0530

    typecasting

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
 @app.get('/')
 def index():
-    return {'data': {'name':'Ansh'}}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/{id}')
+def about(id: int):
+    #fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blogs with id = id
+    return {'data': {'1', '2'}}
