092d9835b1006b861bebe74da5b7105cf1d77354
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-08-09 22:48:11 +0530

    post method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,9 +8,12 @@ app = FastAPI()
 def unpublished():
     return {'data': 'all unpublished blogs'}
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit: int=10, published: bool=True, sort: Optional[str]=None):
+    if(published):
+        return {'data': f'{limit} published articles from the db'}
+    else:
+        return {'data': f'{limit} unpublished articles from the db'}
 
 @app.get('/blog/{id}')
 def about(id: int):
@@ -16,6 +21,16 @@ def about(id: int):
     return {'data': id}
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit: int = 10):
+    # fastapi is smart enough to know the diff between a path parameter and a query parameter
     # fetch comments of blogs with id = id
     return {'data': {'1', '2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+     return {'data': f"Blog is created with title as {blog.title}"}
